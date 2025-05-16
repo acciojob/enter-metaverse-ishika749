6f6874ef4e82d809b59cb7597612a17b8908d9d0
ishika749 <ishikashah749@gmail.com>
2025-05-16 09:19:54 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,4 @@
-//your JS code here. If required.
+document.getElementById("enterBtn").addEventListener("click", function() {
+    let statusElement = document.getElementById("status");
+    statusElement.innerHTML = "<h1>Entered Metaverse</h1>"; // Change text to "Entered Metaverse" within an <h1> tag
+});
